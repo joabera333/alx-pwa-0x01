8f213aa0e3ff0b6d99ddaed14ef82415c8e2c026
jo_abera <johannesab0939@gmail.com>
2025-06-18 16:53:53 +0300

    Add the movies pages to you project

--- alx-movie-app/components/commons/MovieCard.tsx
+++ alx-movie-app/components/commons/MovieCard.tsx
@@ -1,20 +1,25 @@
-import React from "react";
+import { MovieProps } from "@/interfaces";
+import Image from "next/image";
 
-interface MovieCardProps {
-	title: string;
-	posterUrl?: string;
-	year?: number;
-}
-
-const MovieCard: React.FC<MovieCardProps> = ({ title, posterUrl, year }) => {
+const MovieCard: React.FC<MovieProps> = ({
+	title,
+	posterImage,
+	releaseYear,
+}) => {
 	return (
-		<div className="border rounded-lg overflow-hidden shadow-lg">
-			{posterUrl && (
-				<img src={posterUrl} alt={title} className="w-full h-48 object-cover" />
-			)}
-			<div className="p-4">
-				<h3 className="font-bold text-lg">{title}</h3>
-				{year && <p className="text-gray-600">{year}</p>}
+		<div className="h-[563px]">
+			<div>
+				<Image
+					className="h-[430px] w-full rounded-md hover:cursor-pointer"
+					src={posterImage}
+					width={100}
+					height={100}
+					alt={title}
+				/>
+			</div>
+			<div className="flex justify-between py-4">
+				<p className="text-xl font-bold">{title}</p>
+				<p className="text-xl text-[#E2D609]">{releaseYear}</p>
 			</div>
 		</div>
 	);
