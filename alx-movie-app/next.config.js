69df9e26486f988fad44d097e87b40ea1d2871f3
jo_abera <johannesab0939@gmail.com>
2025-07-06 19:15:18 +0300

    Link the Manifest in _document.js

--- alx-movie-app/next.config.js
+++ alx-movie-app/next.config.js
@@ -3,16 +3,16 @@ import withPWAInit from "@ducanh2912/next-pwa";
 /** @type {import('next').NextConfig} */
 
 const withPWA = withPWAInit({
-  dest: 'public'
-})
+	dest: "public",
+});
 
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['m.media-amazon.com'],
-  },
+	reactStrictMode: true,
+	images: {
+		domains: ["m.media-amazon.com"],
+	},
 };
 
 export default withPWA({
-  ...nextConfig
-})
+	...nextConfig,
+});
